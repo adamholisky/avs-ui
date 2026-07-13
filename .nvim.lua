c0e91eaa4a99141bbca1fee4db8d7d20746f3333
Adam Holisky <adam.holisky@gmail.com>
2026-07-13 18:24:32 -0500

    nvim config tests

---
+++ .nvim.lua
@@ -0,0 +1,19 @@
+vim.notify("AVS-UI .nvim.lua loaded!", vim.log.levels.INFO)
+
+-- layout
+require('nvim-tree.api').tree.open()
+
+-- Editor basics
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.expandtab = false
+
+-- Project setup
+vim.g.avs_project_name = 'avs-ui'
+
+
+vim.api.nvim_create_user_command( 'avs_run', function() 
+	vim.cmd('!make run')
+end, {})
+
+
